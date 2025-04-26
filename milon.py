04faa4d2381565b90f0c2cff221da3e2a2ddf0bd
aeoa <64489515+aeoa@users.noreply.github.com>
2025-04-26 13:46:32 +0200

    first commit

---
+++ milon.py
@@ -0,0 +1,341 @@
+import requests
+import json
+import os
+from getpass import getpass
+import datetime
+import pathlib
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from collections import defaultdict
+
+HOST = 'https://www.milonme.com'
+
+DATA_FOLDER = pathlib.Path("data")
+SESSION_FILE = DATA_FOLDER / 'session.json'
+
+GRAPH_FOLDER = pathlib.Path("graphs")
+
+rs = None
+ms = None
+
+def perform_login():
+    print("Login to MilonMe")
+    email = input("E-Mail: "),
+    password = getpass("Password: ")
+
+    rs = requests.Session()
+    
+    try:
+        response = rs.post(
+            f"{HOST}/api/user/login",
+            data={'email': email, 'password': password, 'long_session': 0},
+            headers={'x-api-key': 'v1uCCMWOFj8mbTdtkO7ia76K3h76tuvb2lOrL8RF'}
+            )
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Login error: {e}")
+        return
+    
+    del password
+    
+    session = {
+        'session': response.json(),
+        'cookies': rs.cookies.get_dict()
+    }
+
+    os.makedirs(SESSION_FILE.parent, exist_ok=True)
+    with open(SESSION_FILE, 'w') as f:
+        json.dump(session, f, indent=4)
+
+def load_session():
+    global rs, ms
+    try:
+        with open(SESSION_FILE, 'r') as f:
+            session_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Session file not found or corrupted, please log in")
+        rs = None
+        ms = None
+        return
+        
+    ms = session_data.get('session', None)
+    
+    rs = requests.Session()
+    rs.cookies.update(session_data['cookies'] if 'cookies' in session_data else {})
+
+    response = rs.get(f'{HOST}/api/user/session')
+    if response.status_code != 200:
+        print("Session expired or invalid, please log in again")
+        rs = None
+        ms = None
+
+def delete_session():
+    global rs, ms
+
+    if rs is None:
+        load_session()
+    
+    if rs is not None:
+        rs.delete(f'{HOST}/api/user/session')
+        rs = None
+
+    if os.path.exists(SESSION_FILE):
+        os.remove(SESSION_FILE)
+    ms = None
+
+def establish_session():
+    while True:
+        load_session()
+        if rs is not None:
+            break
+        perform_login()
+
+def load_stats_home():
+    studio_id = ms['d']['studios']
+    user_id = ms['id']
+    response = rs.get(f'{HOST}/api/user/stats/home/{studio_id}/{user_id}')
+    response.raise_for_status()
+
+    output_folder = DATA_FOLDER / user_id / 'stats'
+    os.makedirs(output_folder, exist_ok=True)
+    with open(output_folder / 'home.json', 'w') as f:
+        f.write(response.text)
+
+    home = response.json()
+    print(f"Welcome back {home['profile']['firstname']} {home['profile']['lastname']}!")
+    print(f"Studio: {home['studio']['studioname']}")
+
+def load_devices():
+    r = rs.get(f'{HOST}/api/devices/en_US')
+    r.raise_for_status()
+
+    os.makedirs(DATA_FOLDER, exist_ok=True)
+    with open(DATA_FOLDER / 'devices.json', 'w') as f:
+        f.write(r.text)
+
+def load_stats_premium():
+    studio_id = ms['d']['studios']
+    user_id = ms['id']
+
+    output_folder = DATA_FOLDER / user_id / 'stats' / 'premium'
+    os.makedirs(output_folder, exist_ok=True)
+
+    now = datetime.datetime.now()
+    y = now.year % 100
+    m = now.month
+
+    # Get the last year and month that was already downloaded
+    files = sorted(output_folder.glob('*.json'))
+    if files:
+        last_file = files[-1]
+        last_yymm = os.path.basename(last_file)[:4]
+    else:
+        last_yymm = None
+
+    print('Downloading premium stats for the last 12 months:', end='', flush=True)
+    for i in range(13):
+        yymm = f'{y:02}{m:02}'
+        print(f" {yymm}", end='', flush=True)
+
+        response = rs.get(f'{HOST}/api/user/stats/premium/{studio_id}/{user_id}/{yymm}')
+        response.raise_for_status()
+
+        with open(output_folder / f'{yymm}.json', 'w') as f:
+            f.write(response.text)
+
+        if last_yymm and yymm == last_yymm:
+            print('\nSkipping previous months since they were already fully downloaded')
+            break
+        
+        # decrement month
+        m -= 1
+        if m == 0:
+            m = 12
+            y -= 1
+    print()
+
+def plot_weights(data, devices, output_folder, ids):
+    """Plottet Eccentric und Concentric als Gewichtsdarstellung und speichert weights.png"""
+    fig, axes = plt.subplots(nrows=3, ncols=4, sharex=True)
+    fig.subplots_adjust(left=0.04, bottom=0.05, right=0.98, top=0.965, wspace=0.15, hspace=0.28)
+    fig.set_size_inches(16, 9)
+    for ax in axes.flat:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+        ax.xaxis.set_major_locator(mdates.MonthLocator())
+        ax.yaxis.get_major_locator().set_params(integer=True)
+        ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}'))
+    for idx, device_id in enumerate(ids):
+        row = idx // axes.shape[1]
+        col = idx % axes.shape[1]
+        ax = axes[row, col]
+        # Filter für Gerät
+        data_id = data[data["device"] == device_id]
+        ax.plot(data_id["time"], data_id["eccentric"], label="Eccentric")
+        ax.plot(data_id["time"], data_id["concentric"], label="Concentric")
+        ax.set_title(devices[str(device_id)]["name"])
+        if row == axes.shape[0] - 1:
+            ax.set_xlabel("Time")
+        if col == 0:
+            ax.set_ylabel("Weight / kg")
+        ax.legend(loc='lower right')
+    plt.savefig(output_folder / "weights.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+def plot_delta_percentage(data, devices, output_folder, ids):
+    """Plottet den prozentualen Unterschied zwischen Eccentric und Concentric und speichert delta percentage.png"""
+    fig, axes = plt.subplots(nrows=3, ncols=4, sharex=True, sharey=True)
+    fig.subplots_adjust(left=0.04, bottom=0.05, right=0.98, top=0.965, wspace=0.15, hspace=0.28)
+    fig.set_size_inches(16, 9)
+    for ax in axes.flat:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+        ax.xaxis.set_major_locator(mdates.MonthLocator())
+        ax.yaxis.get_major_locator().set_params(integer=True)
+        ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}'))
+    for idx, device_id in enumerate(ids):
+        row = idx // axes.shape[1]
+        col = idx % axes.shape[1]
+        ax = axes[row, col]
+        data_id = data[data["device"] == device_id]
+        ax.axhline(y=20, color='r', linestyle='--', label="20%")
+        percentage = (data_id["eccentric"] / data_id["concentric"] - 1) * 100
+        ax.plot(data_id["time"], percentage, label="delta %")
+        ax.set_title(devices[str(device_id)]["name"])
+        if row == axes.shape[0] - 1:
+            ax.set_xlabel("Time")
+        if col == 0:
+            ax.set_ylabel("Percentage")
+        ax.legend(loc='lower right')
+    plt.savefig(output_folder / "delta percentage.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+def plot_reps(data, devices, output_folder, ids):
+    """Plottet Wiederholungen (reps) und speichert reps.png"""
+    fig, axes = plt.subplots(nrows=3, ncols=4, sharex=True, sharey=True)
+    fig.subplots_adjust(left=0.04, bottom=0.05, right=0.98, top=0.965, wspace=0.15, hspace=0.28)
+    fig.set_size_inches(16, 9)
+    for ax in axes.flat:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+        ax.xaxis.set_major_locator(mdates.MonthLocator())
+        ax.yaxis.get_major_locator().set_params(integer=True)
+        ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}'))
+    for idx, device_id in enumerate(ids):
+        row = idx // axes.shape[1]
+        col = idx % axes.shape[1]
+        ax = axes[row, col]
+        data_id = data[data["device"] == device_id]
+        ax.axhline(y=8, color='r', linestyle='--', label="8")
+        ax.plot(data_id["time"], data_id["moves"], label="reps")
+        ax.set_title(devices[str(device_id)]["name"])
+        if row == axes.shape[0] - 1:
+            ax.set_xlabel("Time")
+        if col == 0:
+            ax.set_ylabel("Repetitions")
+        ax.legend(loc='lower right')
+    plt.savefig(output_folder / "reps.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+def plot_work(data_training_accumulated, devices, output_folder, ids):
+    """Plottet Work anhand von 2 oder 3 Sätzen und speichert work.png"""
+    fig, axes = plt.subplots(nrows=3, ncols=4, sharex=True)
+    fig.subplots_adjust(left=0.04, bottom=0.05, right=0.98, top=0.965, wspace=0.15, hspace=0.28)
+    fig.set_size_inches(16, 9)
+    for ax in axes.flat:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
+        ax.xaxis.set_major_locator(mdates.MonthLocator())
+        ax.yaxis.get_major_locator().set_params(integer=True)
+        ax.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}'))
+    for idx, device_id in enumerate(ids):
+        row = idx // axes.shape[1]
+        col = idx % axes.shape[1]
+        ax = axes[row, col]
+        data_id_accumulated_3 = data_training_accumulated[(data_training_accumulated["device"] == device_id) & (data_training_accumulated["sets"] == 3)]
+        ax.plot(data_id_accumulated_3["training"], data_id_accumulated_3["work"], label="3 sets")
+        data_id_accumulated_2 = data_training_accumulated[(data_training_accumulated["device"] == device_id) & (data_training_accumulated["sets"] == 2)]
+        ax.plot(data_id_accumulated_2["training"], data_id_accumulated_2["work"], label="2 sets")
+        ax.set_title(devices[str(device_id)]["name"])
+        if row == axes.shape[0] - 1:
+            ax.set_xlabel("Time")
+        if col == 0:
+            ax.set_ylabel("Work / kWs")
+        ax.legend(loc='lower right')
+    plt.savefig(output_folder / "work.png", dpi=300, bbox_inches='tight')
+    plt.close(fig)
+
+def plot_all():
+    """Führt alle Plot-Funktionen aus."""
+    user_id = ms['id']
+    output_folder = GRAPH_FOLDER / user_id
+    os.makedirs(output_folder, exist_ok=True)
+    # Geräte laden
+    with open(DATA_FOLDER / "devices.json", 'r') as file:
+        devices = json.load(file)
+    # IDs für die Plots
+    ids = [22, 17, 5, 6, 11, 19, 21, 10, 14, 13, 15, 12]
+
+    # Datenaufbereitung (wie bisher)
+    # Daten aus Premium-Stats zusammenfassen
+    data = pd.DataFrame(columns=["training", "set", "device", "time", "duration", "moves", "concentric", "eccentric", "work"])
+    premium_stats_folder = DATA_FOLDER / ms['id'] / 'stats' / 'premium'
+    for file_path in sorted(premium_stats_folder.glob("*.json")): 
+        print(f"Processing {file_path}")
+        with open(file_path, 'r') as file:
+            jsondata = json.load(file)
+        for training in jsondata["stats"]:
+            device_count = defaultdict(int)
+            for device in training["devices"]:
+                if "moves" not in device:
+                    continue
+                device_count[device["id"]] += 1
+                data.loc[len(data)] = [
+                    training["training"]["t"],
+                    device_count[device["id"]],
+                    device["id"],
+                    device["t"],
+                    device["d"],
+                    device["moves"],
+                    device["aw"],
+                    device["adw"],
+                    device["ws"],
+                ]
+    data.sort_values(by=["time"], inplace=True)
+    data = data[data["time"] > data["time"].min() + 3 * 60 * 60]
+    data = data[~((data["device"] == 15) & (data["concentric"] == 12) & (data["eccentric"] == 14)  & (data["duration"] == 10)  & (data["moves"] == 1))]
+    
+    data_training_accumulated = pd.DataFrame(columns=["training", "sets", "device", "duration", "moves", "concentric", "eccentric", "work"])
+    for training in data["training"].unique():
+        data_training = data[data["training"] == training]
+        for device in data_training["device"].unique():
+            data_device = data_training[data_training["device"] == device]
+            data_training_accumulated.loc[len(data_training_accumulated)] = [
+                training,
+                data_device.shape[0],
+                device,
+                data_device["duration"].sum(),
+                data_device["moves"].sum(),
+                data_device["concentric"].mean(),
+                data_device["eccentric"].mean(),
+                data_device["work"].sum(),
+            ]
+    # Konvertierung in DateTime
+    data["time"] = pd.to_datetime(data["time"], unit='s')
+    data["training"] = pd.to_datetime(data["training"], unit='s')
+    data_training_accumulated["training"] = pd.to_datetime(data_training_accumulated["training"], unit='s')
+
+    # Aufruf der einzelnen Plot-Funktionen
+    plot_weights(data, devices, output_folder, ids)
+    plot_delta_percentage(data, devices, output_folder, ids)
+    plot_reps(data, devices, output_folder, ids)
+    plot_work(data_training_accumulated, devices, output_folder, ids)
+
+def main():
+    # delete_session()
+    establish_session()
+    load_stats_home()
+    load_devices()
+    load_stats_premium()
+    plot_all()
+
+if __name__ == "__main__":
+    main()
